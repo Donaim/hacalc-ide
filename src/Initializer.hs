582966b084cfe98c4569823afc0749b0b909f634
Donaim <3Donaim@gmail.com>
2019-07-30 23:40:47 +0000

    better load and new

--- src/Initializer.hs
+++ src/Initializer.hs
@@ -1,11 +1,13 @@
 
 module Initializer where
 
-import Util
 
 import Control.Concurrent as CONC
 import Control.Concurrent.ParallelIO.Local as CONC
+import Text.Read (readMaybe)
+import Control.Monad
 
+import Util
 import Events
 import SimpleReader
 import SimpleUI
@@ -20,24 +22,44 @@ newSystem readfile uifile = do
 	let cli = userCLINew
 	return (c, r, ui, cli)
 
-systemRun :: (CompilerState, ReaderState, UIState, CLIState) -> IO ()
+loadSystem :: String -> IO (CompilerState, ReaderState, UIState, CLIState)
+loadSystem path = do
+	text <- readFile path
+	case readMaybe text of
+		Nothing -> fail $ "Could not parse text:\n" ++ show text
+		Just pairs -> return pairs
+
+	where
+	parsearr parts = do
+		guard (length parts == 4)
+		c <- readMaybe (parts !! 0)
+		r <- readMaybe (parts !! 1)
+		ui <- readMaybe (parts !! 2)
+		cli <- readMaybe (parts !! 3)
+		return (c, r, ui, cli)
+
+systemRun :: (CompilerState, ReaderState, UIState, CLIState) -> IO String
 systemRun (c, r, ui, cli) = do
 	ebin <- eventsBinNew
 	withEbin ebin
-	return ()
 	where
 	withEbin ebin = do
-		CONC.withPool (length actions) (flip parallel actions)
-		where
-		actions =
-			[ (reactorLoop ebin cli >> return ())
-			, (reactorLoop ebin c >> return ())
-			, (reactorLoop ebin r >> return ())
-			, (reactorLoop ebin ui >> return ())
+		x <- CONC.withPool (length actions) (flip parallel actions)
+		return $ show (x !! 0, x !! 1, x !! 2, x !! 3)
+		where actions =
+			[ (reactorLoop ebin cli >>= return . show)
+			, (reactorLoop ebin c >>= return . show)
+			, (reactorLoop ebin r >>= return . show)
+			, (reactorLoop ebin ui >>= return . show)
 			]
 
-newSystemRun :: String -> String -> IO ()
+newSystemRun :: String -> String -> IO String
 newSystemRun readfile uifile = do
 	states <- newSystem readfile uifile
 	systemRun states
+
+loadSystemRun :: String -> IO String
+loadSystemRun path = do
+	r <- loadSystem path
+	systemRun r
 
