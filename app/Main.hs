f33e574270edb9bb5a89563d0186e633b1502f0f
Donaim <3Donaim@gmail.com>
2019-07-30 21:49:38 +0000

    accept arguments

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,20 @@
 module Main where
 
+import System.Environment
+import Control.Monad
+
 import Initializer
 
 main :: IO ()
 main = do
+	args <- getArgs
+
+	when (length args /= 2) $ do
+		putStrLn $ "Usage: hacalc-ide <rulefile> <displayfile>"
+
+	let rulefile = args !! 0
+	let displayfile = args !! 1
+
 	putStrLn "Starting"
-	newSystemRun "rules~" "display~"
+	newSystemRun rulefile displayfile
 
